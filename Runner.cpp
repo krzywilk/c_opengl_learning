6cbab839efb6e53a1a2408d11e831e23e7819f13
krzywilk <krzywilk@gmail.com>
2024-01-10 15:01:48 +0100

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -4,8 +4,9 @@
 #include <glad/glad.h>
 #include "src/PredefinedWindows/HelloWindow.h"
 #include <vector>
-#include "src/ShapeDrawers/SingelTriangleShaderDrawer.h"
 #include "src/utils/simples.cpp"
+#include "src/ShapeDrawers/TrianglesShaderDrawer.h"
+#include "src/ShapeDrawers/IndicesShapeShaderDrawer.h"
 
 void init_GLFW(int glfw_version_major = 4, int glfw_version_minor = 4) {
     glfwInit();
@@ -21,8 +22,10 @@ int main()
 {
     init_GLFW();
     HelloWindow basicWindowTest = HelloWindow("Separate impl", 800, 600, rgb(0.3f, 0.1f, 0.1f));
-    SingelTriangleShaderDrawer singleTriangleDraw;
-    float vertices[] = {
+
+
+
+    float topLeftRightTriangle[] = {
        -0.7f, -0.2f, 0.0f, // left  
         0.0f, -0.2f, 0.0f, // right 
         0.0f,  0.2f, 0.0f,  // top
@@ -32,24 +35,37 @@ int main()
         0.0f,  0.2f, 0.0f,  // top
 
     };
-    float color1[] = { 1.0f, 0.5f, 0.2f, 1.0f };
 
-    SingelTriangleShaderDrawer singleTriangleDraw2;
-
-    float vertices2[] = {
-        -0.0f, -0.2f, 0.0f, // left  
-        0.7f, -0.2f, 0.0f, // right 
-        0.0f,  -1.0f, 0.0f,  // top
+    float bottomRightTriangle[] = {
+    -0.0f, -0.2f, 0.0f, // left  
+    0.7f, -0.2f, 0.0f, // right 
+    0.0f,  -0.6f, 0.0f,  // top
     };
-    float color2[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+
+    float rectangle_trainglesBased[] = {
+     0.9f,  0.9f, 0.0f,  // top right
+     0.9f, 0.5f, 0.0f,  // bottom right
+     0.5f, 0.5f, 0.0f,  // bottom left
+    0.5f,  0.9f, 0.0f   // top left 
+    };
+    unsigned int rectangle_trainglesBasedIndices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
 
 
-    singleTriangleDraw.transferData(vertices, sizeof(vertices));
-    singleTriangleDraw2.transferData(vertices2, sizeof(vertices2));
+    TrianglesShaderDrawer twoTrainglesDrawer;
+    TrianglesShaderDrawer singleTriangleDrawer;
+    IndicesShapeShaderDrawer indicesRectangleDrawer;
 
-    vector<SingelTriangleShaderDrawer> triangleDrawers;
-    triangleDrawers.push_back(singleTriangleDraw);
-    triangleDrawers.push_back(singleTriangleDraw2);
+    twoTrainglesDrawer.transferData(topLeftRightTriangle, sizeof(topLeftRightTriangle));
+    singleTriangleDrawer.transferData(bottomRightTriangle, sizeof(bottomRightTriangle));
+    indicesRectangleDrawer.transferData(rectangle_trainglesBased, sizeof(rectangle_trainglesBased), rectangle_trainglesBasedIndices, sizeof(rectangle_trainglesBasedIndices));
+
+    vector<ShaderDrawer*> triangleDrawers;
+    triangleDrawers.push_back(&twoTrainglesDrawer);
+    triangleDrawers.push_back(&singleTriangleDrawer);
+    triangleDrawers.push_back(&indicesRectangleDrawer);
     
     basicWindowTest.renderLoop(triangleDrawers);
     glfwTerminate();
